224d1525461948259b29e5a3d811f063e4ce73a9
Maxime RIO <maxime.rio@niwa.co.nz>
2023-07-27 22:48:42 +0000

    move files to adhere more to standard snakemake distribution format
    see https://snakemake.readthedocs.io/en/stable/snakefiles/deployment.html

--- config/heuristic.py
+++ config/heuristic.py
@@ -1,8 +1,9 @@
-import os
-def create_key(template, outtype=('nii.gz',), annotation_classes=None):
+def create_key(template, outtype=("nii.gz",), annotation_classes=None):
     if template is None or not template:
-        raise ValueError('Template must be a valid format string')
+        raise ValueError("Template must be a valid format string")
     return template, outtype, annotation_classes
+
+
 def infotodict(seqinfo):
     """Heuristic evaluator for determining which runs belong where
     allowed template fields - follow python string module:
@@ -11,14 +12,19 @@ def infotodict(seqinfo):
     seqitem: run number during scanning
     subindex: sub index within group
     """
-    t1w = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_run-00{item:01d}_T1w')
-    func_rest = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-rest_run-00{item:01d}_bold')
+    t1w = create_key(
+        "sub-{subject}/{session}/anat/sub-{subject}_{session}_run-00{item:01d}_T1w"
+    )
+    func_rest = create_key(
+        "sub-{subject}/{session}/func/sub-{subject}_{session}_task-rest_run-00{item:01d}_bold"
+    )
 
     info = {t1w: [], func_rest: []}
-    
+
     for idx, s in enumerate(seqinfo):
-        if (s.dim1 == 512) and (s.dim2 == 512) and ('BRAVO' in s.sequence_name):
+        if (s.dim1 == 512) and (s.dim2 == 512) and ("BRAVO" in s.sequence_name):
             info[t1w].append(s.series_id)
-        if (s.dim1 == 64) and (s.dim2 == 64) and ('epiRT' in s.sequence_name):
+        if (s.dim1 == 64) and (s.dim2 == 64) and ("epiRT" in s.sequence_name):
             info[func_rest].append(s.series_id)
+
     return info
